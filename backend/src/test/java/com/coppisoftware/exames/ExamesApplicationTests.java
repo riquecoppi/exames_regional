984e28f5db1534333aa123d60ad7b8a913968131
Henrique C. Ribeiro <riquecoppi@gmail.com>
2022-04-19 15:12:21 -0300

    Projeto Criado

---
+++ backend/src/test/java/com/coppisoftware/exames/ExamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coppisoftware.exames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
